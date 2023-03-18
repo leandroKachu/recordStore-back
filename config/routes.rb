f2f80e210c19861e040e4a11ef94527a71f234c8
leandroKachu <lesilvajunior@hotmail.com>
2023-03-18 13:53:04 -0700

    add module api/v1 and route namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  resources :records
-  resources :artists
+  namespace :api do 
+    namespace :v1 do
+      resources :records
+      resources :artists
+    end
+  end
+
+  root to: "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
