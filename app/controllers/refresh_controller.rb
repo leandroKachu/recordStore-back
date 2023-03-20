758cd309ffb90c27a409d96f5583850c396c68fb
leandroKachu <lesilvajunior@hotmail.com>
2023-03-20 14:45:28 -0700

    add configurations to jwt and sigin

---
+++ app/controllers/refresh_controller.rb
@@ -0,0 +1,18 @@
+class RefreshController < ApplicationController
+  before_action :authorize_refresh_by_access_request!
+
+  def create
+    session = JWTSessions::Session.new(payload: claimless_payload, refresh_by_access_allowed: true)
+    tokens = session.refresh_by_access_allowed do 
+      raise JWTSessions::Errors::Unauthorized, "something went wrong here"
+    end
+
+    response.set_cookie(JWTSessions.accept_cookie,
+                        value: tokens[:access],
+                        httponly: true,
+                        secure: Rails.env.production?)
+
+    render json: {csrf: tokens[:csrf]}                    
+
+  end
+end
